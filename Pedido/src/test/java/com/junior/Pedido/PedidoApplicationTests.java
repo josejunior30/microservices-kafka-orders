b6490e9ce344b6afca554ff9f30448e4d2348de2
juneba <junebajuneba@gmail.com>
2025-12-05 00:39:22 -0300

    first commit

---
+++ Pedido/src/test/java/com/junior/Pedido/PedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.Pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
